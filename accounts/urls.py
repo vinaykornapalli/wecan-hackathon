71e8e851a5821af3df0b09fdcc11cf49e9f8078b
vinaykornapalli <vinaykornapalli@gmail.com>
2018-03-17 13:17:45 +0530

    front end added

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ app_name ="accounts"
 
 urlpatterns = [
     url(r'login/$' , auth_views.LoginView.as_view(template_name = 'accounts/login.html'),name="login"),
-    url(r'logout/$',auth_views.LoginView.as_view(),name="logout"),
+    url(r'logout/$',auth_views.LogoutView.as_view(),name="logout"),
     url(r'signup/$',views.signUp.as_view(template_name="accounts/signup.html"),name="signup")
 
 ]
